ea81cd7b603f185c8ea2c9a2529e4a563af8f88f
Aryan <aryansinha6363@gmail.com>
2020-08-01 23:12:07 +0530

    Exercise 1.1

---
+++ temperature/src/main.rs
@@ -0,0 +1,51 @@
+
+fn main() {
+    println!("---TEMPERATURE CONVERTER---");
+    println!("From which to which ?
+     1) Celsius to Farenheit - Enter 1
+     2) Farenheit to Celsius - Enter 2");
+loop { 
+     let mut val = String::new();
+     std::io::stdin()
+                    .read_line(&mut val)
+                    .expect("Value not found");
+    let val: u64 = match val
+                            .trim()
+                            .parse() {
+                                Ok(num) => num,
+                                Err(_) => continue,
+                            };
+
+    println!("INPUT temperature value: ");
+
+    let mut temp = String::new();
+    std::io::stdin()
+                .read_line(&mut temp)
+                .expect("Value not found");
+    let temp: u64 = match temp
+                            .trim()
+                            .parse() {
+                                Ok(num) => num,
+                                Err(_) => continue,
+                            };
+    
+    if val == 2 {
+        println!("Converting Farenheit to Celsius");
+        let res: u64 = (temp-32) * 5 / 9;
+        println!("{} ", res);
+        break;
+    }
+    
+    else if   val == 1 {
+        println!("Converting Celsius to Farenheit");
+        let res: u64 = (temp * 9 / 5) + 32;
+        println!("{} ", res);
+        break;
+    }
+
+    else {
+        println!("Wrong input BABY");
+        continue;
+    }
+}
+ }
---
+++ temperature/target/release/temperature.d
@@ -0,0 +1,1 @@
+/Users/arx6363/Desktop/rust/temperature/target/release/temperature: /Users/arx6363/Desktop/rust/temperature/src/main.rs
